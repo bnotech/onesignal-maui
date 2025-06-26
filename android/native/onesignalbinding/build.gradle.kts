be713714b821d79e931ed4bf92dbb393c3590711
Bastian Noffer <bastian.noffer@gmail.com>
2025-06-26 08:43:31 +0200

    Refactor: Sample

--- android/native/onesignalbinding/build.gradle.kts
+++ android/native/onesignalbinding/build.gradle.kts
@@ -25,12 +25,34 @@ android {
     }
 }
 
+configurations {
+    create("copyDependencies")
+}
+
 dependencies {
 
     // Add package dependency for binding library
     // Uncomment line below and replace {dependency.name.goes.here} with your dependency
     // implementation("{dependency.name.goes.here}")
     implementation("com.onesignal:OneSignal:5.1.34")
-
+    "copyDependencies"("com.onesignal:OneSignal:5.1.34")
 }
 
+configurations.all {
+    resolutionStrategy {
+        eachDependency {
+            if (requested.group == "org.jetbrains.skiko" && requested.name == "skiko") {
+                useTarget("org.jetbrains.skiko:skiko-android:0.7.7")
+            }
+        }
+    }
+}
+
+// Copy dependencies for binding library. Uncomment code blocks below.
+project.afterEvaluate {
+    tasks.register<Copy>("copyDeps") {
+        from(configurations["copyDependencies"])
+        into("${projectDir}/build/outputs/deps")
+    }
+    tasks.named("preBuild") { finalizedBy("copyDeps") }
+}
